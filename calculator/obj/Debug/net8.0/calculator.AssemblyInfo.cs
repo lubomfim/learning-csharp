f5453670bc238affbbe7c1dd9f5a36bd056023c9
lucas <hello@lubomfim.com>
2023-11-23 13:55:17 +0000

    calculator improvemnts

--- calculator/obj/Debug/net8.0/calculator.AssemblyInfo.cs
+++ calculator/obj/Debug/net8.0/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50fc77dd8cb898bcfd36fa0d161fa1c45130dc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
