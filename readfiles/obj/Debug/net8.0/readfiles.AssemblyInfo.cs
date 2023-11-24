80257ef06e2351ee29e69381c889ebb5e9097c74
lucas <hello@lubomfim.com>
2023-11-24 12:56:49 +0000

    feat: new razor pizza project

--- readfiles/obj/Debug/net8.0/readfiles.AssemblyInfo.cs
+++ readfiles/obj/Debug/net8.0/readfiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("readfiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ddd38b0698bceffb3fe29cd32504a82d522f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("readfiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("readfiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
